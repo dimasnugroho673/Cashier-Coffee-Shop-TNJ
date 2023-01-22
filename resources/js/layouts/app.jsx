ffe66e8c363cf5fbd175d48d1ea024ac8baee641
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2023-01-22 17:36:11 +0700

    improvment ui

--- resources/js/layouts/app.jsx
+++ resources/js/layouts/app.jsx
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
         <Fragment>
             <nav class="navbar navbar-expand-lg bg-white fixed-top shadow-sm">
                 <div class="container" id="navbar-container">
-                    <a class="navbar-brand" id="navbar-brand" href="/frontend/order">{settings.name}</a>
+                    <a class="navbar-brand" id="navbar-brand" href="/frontend/order" style={{ fontWeight: '700' }}>{settings.name}</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
